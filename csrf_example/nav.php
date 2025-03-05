8adfe955ffa8f77bbc39717912824d71928e02c7
Jarrod <me@jarrodrizor.com>
2025-03-05 10:26:43 -0500

    Create nav.php
    Added nav.php

---
+++ csrf_example/nav.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+<style>
+body{
+ width: 980px;
+
+}
+</style>
+</head>
+<body>
+
+    <div class="container">
+        <h1>User Management System</h1>
+        <p>Welcome to the User Management System. Choose an action below:</p>
+        <ul>
+            <li><a href="index.php">Home/Login</a></li>
+            <li><a href="signup.php">Sign Up</a></li>
+            <li><a href="reset_password.php">Reset Your Password</a></li>
+            <li><a href="list_users.php">View All Users</a></li>
+        </ul>
+    </div>
